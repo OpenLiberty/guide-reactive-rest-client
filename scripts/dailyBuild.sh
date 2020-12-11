9bfd2fa4a5c48cba7a35bdb17e122ac7fc348328
Tien Thanh Le <44335153+tt-le@users.noreply.github.com>
2020-12-11 16:12:26 -0500

    Update dailyBuild.sh

--- scripts/dailyBuild.sh
+++ scripts/dailyBuild.sh
@@ -9,6 +9,8 @@ do
     esac
 done
 
+echo "Testing daily build image"
+
 sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/"$DATE"/"$DRIVER"</runtimeUrl></install></configuration>" system/pom.xml inventory/pom.xml query/pom.xml
 cat system/pom.xml inventory/pom.xml query/pom.xml
 
@@ -16,5 +18,15 @@ sed -i "s;FROM openliberty/open-liberty:kernel-java8-openj9-ubi;FROM "$DOCKER_US
 cat system/Dockerfile inventory/Dockerfile query/Dockerfile
 
 docker pull $DOCKER_USERNAME"/olguides:"$BUILD
+
+../scripts/testApp.sh
+
+echo "Testing daily Docker image"
+
+sed -i "s;FROM openliberty/open-liberty:kernel-java8-openj9-ubi;FROM openliberty/daily:latest;g" inventory/Dockerfile system/Dockerfile query/Dockerfile
+
+cat inventory/Dockerfile system/Dockerfile query/Dockerfile
+
+docker pull "openliberty/daily:latest"
 
 ../scripts/testApp.sh
