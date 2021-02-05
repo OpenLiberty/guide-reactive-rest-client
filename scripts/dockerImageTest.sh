4120cdbff7f6e6532a7a545da652837095bcb65f
Austin Seto <austin.seto@ibm.com>
2021-02-05 14:20:23 -0500

    Fix PR Checker errors raised

--- scripts/dockerImageTest.sh
+++ scripts/dockerImageTest.sh
@@ -2,14 +2,15 @@
 while getopts t:d: flag;
 do
     case "${flag}" in
-        t) DATE="${OPTARG}";;
-        d) DRIVER="${OPTARG}";;
+        t) DATE="${OPTARG}" ;;
+        d) DRIVER="${OPTARG}" ;;
+        *) : ;;
     esac
 done
 
 echo "Testing daily Docker image"
 
-sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/"$DATE"/"$DRIVER"</runtimeUrl></install></configuration>" system/pom.xml inventory/pom.xml query/pom.xml
+sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/$DATE/$DRIVER</runtimeUrl></install></configuration>" system/pom.xml inventory/pom.xml query/pom.xml
 cat system/pom.xml inventory/pom.xml query/pom.xml
 
 sed -i "s;FROM openliberty/open-liberty:kernel-java8-openj9-ubi;FROM openliberty/daily:latest;g" system/Dockerfile inventory/Dockerfile query/Dockerfile
