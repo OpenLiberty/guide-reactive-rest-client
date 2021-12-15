ed1396c1878a4f9f5f3197d2a9aba189092686ef
Gilbert Kwan <gkwan@ca.ibm.com>
2021-12-15 13:40:11 -0500

    Version update lmp35 (#123)
    * update versions
    
    * Update lmp, revert to jersey 2.35
    
    Co-authored-by: David Yao <David.Yao@ibm.com>

--- scripts/dailyBuild.sh
+++ scripts/dailyBuild.sh
@@ -15,7 +15,7 @@ echo "Testing daily build image"
 sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/$DATE/$DRIVER</runtimeUrl></install></configuration>" system/pom.xml inventory/pom.xml query/pom.xml
 cat system/pom.xml inventory/pom.xml query/pom.xml
 
-sed -i "s;FROM openliberty/open-liberty:full-java11-openj9-ubi;FROM $DOCKER_USERNAME/olguides:$BUILD;g" system/Dockerfile inventory/Dockerfile query/Dockerfile
+sed -i "s;FROM icr.io/appcafe/open-liberty:full-java11-openj9-ubi;FROM $DOCKER_USERNAME/olguides:$BUILD;g" system/Dockerfile inventory/Dockerfile query/Dockerfile
 cat system/Dockerfile inventory/Dockerfile query/Dockerfile
 
 docker pull "$DOCKER_USERNAME""/olguides:""$BUILD"
