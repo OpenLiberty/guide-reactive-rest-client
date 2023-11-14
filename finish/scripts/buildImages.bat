565c55f9ec32b796aee0b8888f1fcb444eda4a78
root <root@testing1.fyre.ibm.com>
2023-11-14 14:23:51 -0800

    updates scripts to remove zookeeper

--- finish/scripts/buildImages.bat
+++ finish/scripts/buildImages.bat
@@ -1,7 +1,6 @@
 @ECHO Starting Scripts
 @ECHO OFF
 
-@REM start /b docker pull bitnami/zookeeper:3
 start /b docker pull bitnami/kafka:latest
 
 start /b docker build -q -t system:1.0-SNAPSHOT system\.
