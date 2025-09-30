eccd7f305dd09334765a93fcb139ea9e7a146ebd
Gilbert Kwan <gkwan@ca.ibm.com>
2025-09-30 11:29:46 -0400

    Change to use image from cp.stg.icr.io/cp/olc

--- scripts/dockerImageTest.sh
+++ scripts/dockerImageTest.sh
@@ -15,22 +15,22 @@ sed -i "\#</containerRunOpts>#a<install><runtimeUrl>https://public.dhe.ibm.com/i
 cat system/pom.xml inventory/pom.xml query/pom.xml
 
 if [[ "$OL_LEVEL" != "" ]]; then
-  sed -i "s;FROM icr.io/appcafe/open-liberty:kernel-slim-java11-openj9-ubi;FROM cp.stg.icr.io/cp/olc/sps/open-liberty-vnext:$OL_LEVEL-full-java11-openj9-ubi;g" system/Dockerfile inventory/Dockerfile query/Dockerfile
+  sed -i "s;FROM icr.io/appcafe/open-liberty:kernel-slim-java11-openj9-ubi;FROM cp.stg.icr.io/cp/olc/open-liberty-vnext:$OL_LEVEL-full-java11-openj9-ubi;g" system/Dockerfile inventory/Dockerfile query/Dockerfile
 else
-  sed -i "s;FROM icr.io/appcafe/open-liberty:kernel-slim-java11-openj9-ubi;FROM cp.stg.icr.io/cp/olc/sps/open-liberty-daily:full-java11-openj9-ubi;g" system/Dockerfile inventory/Dockerfile query/Dockerfile
+  sed -i "s;FROM icr.io/appcafe/open-liberty:kernel-slim-java11-openj9-ubi;FROM cp.stg.icr.io/cp/olc/open-liberty-daily:full-java11-openj9-ubi;g" system/Dockerfile inventory/Dockerfile query/Dockerfile
   fi
 sed -i "s;RUN features.sh;#RUN features.sh;g" system/Dockerfile inventory/Dockerfile query/Dockerfile
 cat system/Dockerfile inventory/Dockerfile query/Dockerfile
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin cp.stg.icr.io
 if [[ "$OL_LEVEL" != "" ]]; then
-  docker pull -q "cp.stg.icr.io/cp/olc/sps/open-liberty-vnext:$OL_LEVEL-full-java11-openj9-ubi"
+  docker pull -q "cp.stg.icr.io/cp/olc/open-liberty-vnext:$OL_LEVEL-full-java11-openj9-ubi"
   echo "build level:"
-  docker inspect --format "{{ index .Config.Labels \"org.opencontainers.image.revision\"}}" "cp.stg.icr.io/cp/olc/sps/open-liberty-vnext:$OL_LEVEL-full-java11-openj9-ubi"
+  docker inspect --format "{{ index .Config.Labels \"org.opencontainers.image.revision\"}}" "cp.stg.icr.io/cp/olc/open-liberty-vnext:$OL_LEVEL-full-java11-openj9-ubi"
 else
-  docker pull -q "cp.stg.icr.io/cp/olc/sps/open-liberty-daily:full-java11-openj9-ubi"
+  docker pull -q "cp.stg.icr.io/cp/olc/open-liberty-daily:full-java11-openj9-ubi"
   echo "build level:"
-  docker inspect --format "{{ index .Config.Labels \"org.opencontainers.image.revision\"}}" "cp.stg.icr.io/cp/olc/sps/open-liberty-daily:full-java11-openj9-ubi"
+  docker inspect --format "{{ index .Config.Labels \"org.opencontainers.image.revision\"}}" "cp.stg.icr.io/cp/olc/open-liberty-daily:full-java11-openj9-ubi"
 fi
 
 ../scripts/testApp.sh
